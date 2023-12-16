dccf41a5ad5e6fb133eb41273c42d5a95c012647
YehorSlivinskiy <egorslivik@gmail.com>
2023-12-16 20:39:45 +0300

    kvmkw

--- cart.js
+++ cart.js
@@ -1,31 +1,41 @@
-document.querySelector("#cart").addEventListener("click", ()=>{
+document.querySelector("#cart").addEventListener("click", () => {
     document.querySelector("#cart-products").classList.toggle("hide")
 })
 
 let cartProd = document.querySelector("#cart-products")
 
 let cart = []
-if(localStorage.getItem("cart")){
-    cart = JSON.parse (localStorage.getItem("cart"))
+if (localStorage.getItem("cart")) {
+    cart = JSON.parse(localStorage.getItem("cart"))
+    drawCartProducts();
 }
 
-function addProductToCart(id){
-    let product = productsArray.find(function(el){
+function addProductToCart(id) {
+    let product = productsArray.find(function (el) {
         return el.id == id
     })
     cart.push(product)
     console.log(cart)
     localStorage.setItem("cart", JSON.stringify(cart))
+    drawCartProducts();
 }
 
-function drawCartProducts(){
-    if(cart.length == 0) return (cartProd.innerHTML = "cart is empty")
+function drawCartProducts() {
+    if (cart.length == 0) return (cartProd.innerHTML = "cart is empty")
     cartProd.innerHTML = ""
-    cart.forEach((el)=>{
-        cartProd +=`
+    cart.forEach((el) => {
+        cartProd.innerHTML += `
         <p>
             <img src="${el.img}"
+            <span>${el.name}</span>
+            <span>$${el.price}</span>
         </p>
-`
+    `
     })
+    cartProd.innerHTML += `<button onclick="clearCart()">Clear</button>`
+}
+function clearCart() {
+    cart = []
+    localStorage.setItem("cart", "")
+    drawCartProducts()
 }
